f61785d9b8944a518056ebf6881897f99fa381c1
kutschool-PC\mh123hack <mhermens@student.scalda.nl>
2017-03-31 11:59:08 +0200

    change in language to english and added more information and added a beep

--- bitcoin_currency/src/currency/Currency.java
+++ bitcoin_currency/src/currency/Currency.java
@@ -5,6 +5,7 @@
  */
 package currency;
 
+import java.awt.Toolkit;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -30,11 +31,12 @@ public class Currency {
      */
     static Timer timer = new Timer();
     static SimpleDateFormat format = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
-    static double amountbtc_bitstamp = 0.03692232;
-    static double amountbtc_btc_e = 0;
-    static double amounteur_btce = 0;
-    static double amounteur_bitstamp = 0.01;
-    static double amounteur_beginwaarde = 39.16;
+    static double amountbtc_bitstamp = 0.02591176; // amount BTC on bitstamp
+    static double amountbtc_btc_e = 0; // amount BTC on btc-e
+    static double amounteur_btce = 0; // amount euro's on btc-e
+    static double amounteur_bitstamp = 10.01; // amount euro's on bitstamp
+    static double amounteur_beginworth = 39.16; // amount before you started like you begun with 40 euro.
+    static double minimal_btcworth = 1060; // amount before you think you have enough. if its on this amount make a beep.
     //static double gekost = 0 - (amounteur_btce + amounteur_bitstamp);
 
     public static void main(String[] args) throws MalformedURLException, IOException {
@@ -61,8 +63,8 @@ public class Currency {
                 String[] b = in2.readLine().split(",");
                 double amount2 = Double.parseDouble(b[7].substring(7).replace("\"", "").trim()) * amountbtc_btc_e;
 
-                double winst = amount + amount2 + amounteur_bitstamp + amounteur_btce - amounteur_beginwaarde;
-                double winstprocent = (amount + amount2 + amounteur_bitstamp + amounteur_btce) / amounteur_beginwaarde * 100 - 100;
+                double winst = amount + amount2 + amounteur_bitstamp + amounteur_btce - amounteur_beginworth;
+                double winstprocent = (amount + amount2 + amounteur_bitstamp + amounteur_btce) / amounteur_beginworth * 100 - 100;
 //                if (winst > 0) {
 //                    try {
 //                        Clip clip = AudioSystem.getClip();
@@ -74,11 +76,15 @@ public class Currency {
 //                        System.err.println(e.getMessage());
 //                    }
 //                }
-                System.out.println("winst: " + winst);
-                System.out.println("winst in %: %" + winstprocent);
-                System.out.println("btc bitstamp waarde: " + a[7].substring(7).replace("\"", "").trim());
-                System.out.println("btc btc-e waarde: " + b[7].substring(7).replace("\"", "").trim());
-                System.out.println("tijd op het momment: " + sdf.format(cal.getTime()));
+                double btcworth = Double.parseDouble(a[7].substring(7).replace("\"", "").trim());
+                if (btcworth > minimal_btcworth){
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                System.out.println("profit: " + winst);
+                System.out.println("profit in %: %" + winstprocent);
+                System.out.println("btc bitstamp worth: " + a[7].substring(7).replace("\"", "").trim());
+                System.out.println("btc btc-e worth: " + b[7].substring(7).replace("\"", "").trim());
+                System.out.println("time on the momend: " + sdf.format(cal.getTime()));
                 System.out.println("___________________");
             } catch (Exception e) {
                 System.err.println("faild to connect will retry in 5.5 sec");
